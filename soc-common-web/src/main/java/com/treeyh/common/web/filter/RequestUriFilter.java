543f52c8303ee68ed321a697665c05b66e5fb479
Tree <tree@ejyi.com>
2019-11-25 17:02:09 +0800

    调整健康检查路径

--- soc-common-web/src/main/java/com/treeyh/common/web/filter/RequestUriFilter.java
+++ soc-common-web/src/main/java/com/treeyh/common/web/filter/RequestUriFilter.java
@@ -42,7 +42,7 @@ public class RequestUriFilter extends BaseFilter {
         //获取相对URI
         String requestURI = httpServletRequest.getRequestURI();
 
-        if("/checkHealth".equals(requestURI)){
+        if("/health".equals(requestURI)){
             //心跳检测,直接返回
             if(!SocCommonWebConfig.SERVICE_RUN_STATUS.equals(SocCommonWebConfig.SERVICE_RUN_STATUS_STARTED)){
                 //判断应用当前是否为不可用状态，如果是那么返回心跳 406 消息，从便于负载均衡摘除
